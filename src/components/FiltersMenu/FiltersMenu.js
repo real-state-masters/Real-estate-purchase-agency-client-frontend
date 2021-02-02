9a499d3481d555488694a2272bd213efec86cb7c
Henrique <henriquepenaconde@gmail.com>
2021-02-02 20:11:12 +0000

    add: dropdown menu sort by on results page

--- src/components/FiltersMenu/FiltersMenu.js
+++ src/components/FiltersMenu/FiltersMenu.js
@@ -6,6 +6,22 @@ import {ReactComponent as Map} from '../../assets/map.svg'
 import {ReactComponent as DownArrow} from '../../assets/down-arrow.svg'
 
 const FiltersMenu = () => {
+    const [sortMenu, setSortMenu] = React.useState(false);
+
+    // handle filter states 
+    const [selectedSort, setSelectedSort] = React.useState('Lowest Price');
+    function handleClick({target}){
+        setSelectedSort(target.innerText);
+        const options = [...document.querySelectorAll('.sortMenu li')];
+        options.forEach((item) =>{
+            if(item.innerText === target.innerText){
+                item.classList.add('sortActive')
+            } else{
+                item.classList.remove('sortActive')
+            }
+        })
+    }
+
     return (
         <div className="filters-menu">
         <div className="filters">
@@ -18,8 +34,16 @@ const FiltersMenu = () => {
         </div>
         <div className="sort-by">
             <span className="sort">Sort by:</span>
-            <span className="ml-1 sort-choice">Lowest Price</span>
-            <DownArrow  className="ml-1"/>
+            <span className="ml-1 sort-choice" onClick={() => setSortMenu(!sortMenu)}>{selectedSort}</span>
+            <DownArrow  className={`down-arrow ml-1 ${sortMenu ? 'up' : 'down'}`} onClick={() => setSortMenu(!sortMenu)}/>
+            <div className={`${sortMenu ? 'sortMenu' : 'hideMenu'}`}>
+                <ul onClick={(e) => handleClick(e)}>
+                    <li className="sortActive">Lowest Price</li>
+                    <li>Highest Price</li>
+                    <li>Oldest</li>
+                    <li>Newest</li>
+                </ul>
+            </div>
         </div>
     </div>
     )
