fe511bcb9d1dc9010eeefbe2d95104228fcbfd24
Henrique <henriquepenaconde@gmail.com>
2021-02-02 17:24:30 +0000

    optimize: improving styles of the HomeOfficeButton component

--- src/components/HomeOfficeButton/HomeOfficeButton.js
+++ src/components/HomeOfficeButton/HomeOfficeButton.js
@@ -2,12 +2,27 @@ import React from 'react'
 import './HomeOfficeButton.scss'
 
 const HomeOfficeButton = () => {
+
+    let [home, setHome] = React.useState(true)
+    let [office, setOffice] = React.useState(false)
+
+    function handleClick({target}){
+        if(target.innerText === 'Home'){
+            setHome(true)
+            setOffice(false)
+        } else{
+            setHome(false)
+            setOffice(true)
+        }
+    }
+
+
     return (
         <div className="home-office-btn">
-                <p className="home active">
+                <p className={`home ${home ?'active' : ''}`} onClick={(e) => handleClick(e)}>
                     Home
                 </p>
-                <p className="office">
+                <p className={`home ${office ?'active' : ''}`} onClick={(e) => handleClick(e)}>
                     Office
                 </p>
             </div>
